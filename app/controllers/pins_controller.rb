1bb58bb267cb12bd040f9ae78eee6da4cf08a3cf
Teddy Kentor <tkentor@gmail.com>
2015-11-13 12:42:20 -0500

    fourth commit, create boards

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -15,11 +15,13 @@ end
 def edit
 end
 
-
+def pin_params
+ params.require(:pin).permit(:title, :image_url)
+end
 
 def create
   @pin = Pin.create!(pin_params)
-  redirect_to pin_path(@pins)
+  redirect_to pin_path(@pin)
  end
 
  def show
